222310024f8a2724a169777fbf308c389c1bfb35
remsta11 <remyhetram11@gmail.com>
2020-11-07 22:59:03 -0500

    timer script and prefab, string drawing test

---
+++ Assets/Scripts/stars.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class stars : MonoBehaviour
+{
+    public Transform[] stars1;
+    public Color normalColor;
+    public Color selectedColor;
+    public Vector3 cursor;
+    public Camera mainCamera;
+
+    Transform transform1;
+    private void Start()
+    {
+        //generate star positions
+        float x = Screen.width;
+        float y = Screen.height;
+        
+        float screenRatio = x / y;
+
+        foreach (Transform pos in stars1)
+        {
+            pos.GetComponent<SpriteRenderer>().color = normalColor;
+            Vector3 newPosition;
+            newPosition.x = Random.Range(-mainCamera.orthographicSize * screenRatio, mainCamera.orthographicSize * screenRatio);
+            newPosition.y = Random.Range(-mainCamera.orthographicSize, mainCamera.orthographicSize);
+            newPosition.z = 0;
+            pos.position = newPosition;
+        }
+    }
+    private void Update()
+    {
+        //temporary, multiple inputs required
+        cursor = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        cursor.z = 0;
+
+        Transform selected = stars1[0];
+        float distance = Vector3.Distance(cursor, stars1[0].position);
+
+        foreach (Transform pos in stars1)
+        {
+            pos.GetComponent<SpriteRenderer>().color = normalColor;
+            float newDistance = Vector3.Distance(cursor, pos.position);
+            if (newDistance < distance)
+            {
+                distance = newDistance;
+                selected = pos;
+            }
+        }
+        selected.GetComponent<SpriteRenderer>().color = selectedColor;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform1 = selected;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            //temporary
+            Debug.DrawLine(transform1.position, cursor);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (!transform1.Equals(selected))
+            {
+                Debug.DrawLine(transform1.position, selected.position, normalColor, 10);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class timer : MonoBehaviour
+{
+    public int timerTime;
+    public int lowTime;
+    public Color normalColor;
+    public Color lowColor;
+    public Text timerText;
+    public UnityEvent onTimerZero;
+
+    float time;
+    void Start()
+    {
+        time = timerTime;
+        timerText.color = normalColor;
+
+        if (lowTime > timerTime)
+            lowTime = timerTime;
+    }
+
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            timerText.text = time.ToString("0");
+        }
+
+        if (time.ToString("0") == lowTime.ToString("0"))
+            timerText.color = lowColor;
+
+        if (time <= 0)
+        {
+            //Debug.Log("Time's Up!");
+            onTimerZero.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}
